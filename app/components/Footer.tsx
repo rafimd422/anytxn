ed259761e770ca7cafd40304fc05f13bf099577e
MD Mushfiqur Rahman <rafimd2222@gmail.com>
2025-02-03 03:29:00 +0600

    Add TypeScript types

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,38 +2,27 @@ import React from 'react';
 import {
   Box,
   Container,
-  Grid,
-  Link,
   Typography,
-  IconButton,
-  List,
-  ListItem,
 } from '@mui/material';
-import { Facebook, Instagram, Twitter, LinkedIn } from '@mui/icons-material';
 
-export const Footer = () => {
+export const Footer: React.FC = () => {
   return (
     <Box
       component="footer"
       sx={{
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'light'
-            ? theme.palette.grey[800]
-            : theme.palette.grey[900],
+        backgroundColor: (theme) => theme.palette.grey[800],
         color: 'white',
         py: 6,
         borderTop: '4px solid',
-        borderColor: 'primary.main',
       }}
     >
       <Container maxWidth="lg">
-
-        <Box mt={5} sx={{display:'flex', justifyContent:'space-between'}}>
+        <Box mt={5} sx={{ display: 'flex', justifyContent: 'space-between' }}>
           <Typography variant="body2" fontWeight={'bold'}>
-          ©2023 All rights reserved. Any Technology Pte Ltd.
+            ©2023 All rights reserved. Any Technology Pte Ltd.
           </Typography>
           <Typography variant="body2" fontWeight={'bold'}>
-          Privecy Policy
+            Privacy Policy
           </Typography>
         </Box>
       </Container>
